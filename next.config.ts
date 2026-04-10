0569826da030def9e90a4e7054b2658ace9d0766
Basti Vitzthum <basti@MBP-von-Basti.fritz.box>
2026-04-11 01:05:51 +0200

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   devIndicators: false,
   output: 'export',
   images: { unoptimized: true },
+  basePath: '/basti-portfolio',
+  assetPrefix: '/basti-portfolio',
 };
 
 export default nextConfig;
